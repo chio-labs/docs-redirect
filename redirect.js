5963b63ddf1d8a7925bffbc5ff211669a56c3279
Kevin Longe <273747162+kvlonge@users.noreply.github.com>
2026-09-26 03:44:25 +0100

    chore: remove the unused redirect script

--- redirect.js
+++
@@ -1,9 +0,0 @@
-function target(pathname, hash) {
-	var path = pathname.replace(/\/index\.html$/, '/').replace(/\/+$/, '');
-	if (path === '') return 'https://sqlbuild.com/';
-	if (/^\/(concepts\/virtual-environments|cli\/(promote|rollback|reconcile|state))(\/|$)/.test(path)) {
-		return 'https://sqlbuild.com/docs/';
-	}
-	if (/\.[a-z0-9]+$/i.test(path)) return 'https://sqlbuild.com' + path;
-	return 'https://sqlbuild.com/docs' + path + '/' + (hash || '');
-}
